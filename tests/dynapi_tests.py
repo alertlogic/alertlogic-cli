6945503650a055929e9541f3e2e759abb33ab525
Dan Greening <daniel.greening@alertlogic.com>
2018-02-20 11:23:22 +0000

    fix E122 and E124 issues

--- tests/dynapi_tests.py
+++ tests/dynapi_tests.py
@@ -17,8 +17,7 @@ def test_parse_apidoc():
             "name": "test_endpoint1",
             "type": "get",
             "url": "/test_service/endpoint1"
-        },
-        {
+        }, {
             "name": "test_endpoint2",
             "type": "post",
             "url": "/test_service/endpoint2"
