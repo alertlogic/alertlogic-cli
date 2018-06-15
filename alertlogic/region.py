e379d0130b1389f12d36dc017acee3e19d5cf964
Finlay Armstrong-Shepherd <finlay.shepherd@AL-CAR-09345.local>
2018-06-15 15:14:19 +0100

    Remove integration

--- alertlogic/region.py
+++ alertlogic/region.py
@@ -9,8 +9,7 @@ List of known regions
 """
 REGIONS = {
     "us": "https://api.cloudinsight.alertlogic.com",
-    "uk": "https://api.cloudinsight.alertlogic.co.uk",
-    "int": "https://api.product.dev.alertlogic.com"
+    "uk": "https://api.cloudinsight.alertlogic.co.uk"
 }
 
 
