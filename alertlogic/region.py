dcdc2ea9948b2dec2f273761a3c161340b0548f5
Anton Benkevich <abenkevich@alertlogic.com>
2019-11-01 14:17:20 +0000

    Do not add api version if not defined Allow to have separate authentication and API endpoints

--- alertlogic/region.py
+++ alertlogic/region.py
@@ -18,11 +18,14 @@ class Region():
     Abstracts an alertlogic region, for now it only represents the api endpoint url
     """
 
-    def __init__(self, api_endpoint):
+    def __init__(self, api_endpoint, auth_endpoint=None):
         """
         :param api_endpint: either a region ("us" or "uk") or an insight api url
         """
+        if auth_endpoint is None:
+            auth_endpoint = api_endpoint
         self._api_endpoint = api_endpoint
+        self._auth_endpoint = auth_endpoint
 
     def get_api_endpoint(self):
         """
@@ -32,3 +35,12 @@ class Region():
             return REGIONS[self._api_endpoint]
         else:
             return self._api_endpoint
+
+    def get_auth_endpoint(self):
+        """
+        returns the region's api endpoint url
+        """
+        if self._api_endpoint in REGIONS:
+            return REGIONS[self._auth_endpoint]
+        else:
+            return self._auth_endpoint
